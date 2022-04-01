66e7e416801e49a59228a5a40aaf506678cbdc86
João Antônio de Castro <joaoaccastro@gmail.com>
2022-04-01 12:11:03 -0300

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,3 +1,5 @@
+var produtos = [];
+
 function addItem(){
     var listaItem = document.getElementById("listaItem");
     var listaQuantidade = document.getElementById("listaQuantidade");
@@ -5,6 +7,10 @@ function addItem(){
     var listaTotalProduto = document.getElementById("listaTotalProduto");
 
     var totalCompra = document.getElementById("valorTotalCompra");
+
+    var totalProdutos = document.getElementById("quantTotalProdutos");
+
+    var totalItens = document.getElementById("quantTotalItens");
 
     var produto = {
         item: document.getElementById("item").value,
@@ -15,18 +21,42 @@ function addItem(){
             document.getElementById("vlrUnitInput").value
     };
 
-    console.log(produto)
-
+    produtos.push(produto);
+    
     var item = '<li>' + produto.item + '</li>';
     var quantidade = '<li>' + produto.quantidade + '</li>';
     var valorUnitario = '<li> R$ ' + produto.valorUnitario + '</li>';
-    var subTotal = '<li> R$ ' + produto.totalItem + '</li>';
-    
-    listaItem.innerHTML += item;
+    var subTotal = '<li> R$ ' + produto.totalItem.toFixed(2) + '</li>';
+
+    listaItem.innerHTML += item.toUpperCase();
     listaQuantidade.innerHTML += quantidade;
     listaVlrUnit.innerHTML += valorUnitario;
     listaTotalProduto.innerHTML += subTotal;
 
-    totalCompra.innerHTML += produto.totalItem;
+    totalProdutos.innerHTML = produtos.length;
 
+    console.log(typeof(subTotal));
+    
+    var total = produtos.reduce(getTotal, 0);
+    function getTotal(total, item){
+        return total + (item.valorUnitario * item.quantidade);
+    }
+
+    // fazer a soma da quantidade
+
+    var totalQuantidade = 0;
+    totalQuantidade += parseFloat(produto.quantidade);
+
+    totalCompra.innerHTML = ''
+    totalCompra.innerHTML = total.toFixed(2);
+
+    totalItens.innerHTML = totalQuantidade;
+
+    document.getElementById("item").value = '';
+    document.getElementById("item").focus();
+
+    document.getElementById("qtdeInput").value = '';
+    document.getElementById("vlrUnitInput").value = '';
+
+    console.log(produtos)
 }
